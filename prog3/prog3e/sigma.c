ec19850b50187bf8c5b47af6a9d04d4e124aa2fb
Brian McIlwain <brianwmcilwain@gmail.com>
2016-10-03 12:00:12 -0500

    added description

--- prog3/prog3e/sigma.c
+++ prog3/prog3e/sigma.c
@@ -1,3 +1,13 @@
 // Brian McIlwain
-// Oper Sys hw 3
-// 
+// Op Sys hw 3
+// Sums and products
+//  Using OpenMP, write a program which accepts integer start, stop, and step values 
+//  (in that order) as command-line arguments and calculates in parallel both the sum and 
+//  product from start to stop (inclusive) in increments of step. Be sure to use the 
+//  proper reductions, and check to be sure that the results are correct. Call this sigma.c.
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
