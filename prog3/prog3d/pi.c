8531d967835b447c1e3ffcb4030a688d081f6837
Brian McIlwain <brianwmcilwain@gmail.com>
2016-10-04 14:05:38 -0500

    Almost done 3d

--- prog3/prog3d/pi.c
+++ prog3/prog3d/pi.c
@@ -11,10 +11,47 @@
 //  results. Use t=10000 for each thread. Call this pi.c. Note that you will need to 
 //  link against the pthreads library when compiling.
 
-#include<sdtio.h>
-#include<pthreads>
+#include<stdio.h>
+#include<pthread.h>
+
+typedef struct {
+    int t;
+    long double ans;
+} pi_args;
+
+void * pi(void * void_args);
 
 int main() {
+    // Prep threads
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    pthread_t tid[4];
+
+    pi_args args[4] = {{ .t = 10000, .ans = 0 }, { .t = 10000, .ans = 0 }, { .t = 10000, .ans = 0 }, { .t = 10000, .ans = 0 }};
+
+    int i;
+    for (i = 0; i < 4; ++i)
+        pthread_create(&tid[i], &attr, pi, &args[i]);
+
+    for (i = 0; i < 4; ++i)
+        pthread_join(tid[i], NULL);
+
+    long double pi = 0;
+    for (i = 0; i < 4; ++i)
+        pi += args[i].ans;
+
+    pi /= 4;
+
+    printf("pi: %.50Lf\n", pi);
 
     return 0;
+}
+
+void * pi(void * void_args) {
+    pi_args* args = (pi_args *)void_args;
+    
+    args->ans = 0;
+
+    return NULL;
 }
