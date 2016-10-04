a84fdb109f2c5d42762d256fdfd80e75c7382874
Brian McIlwain <brianwmcilwain@gmail.com>
2016-10-04 18:02:01 -0500

    Finished 3d

--- prog3/prog3d/pi.c
+++ prog3/prog3d/pi.c
@@ -12,12 +12,17 @@
 //  link against the pthreads library when compiling.
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
+#include<math.h>
 
 typedef struct {
-    int t;
+    long long t;
     long double ans;
 } pi_args;
+
+#define T_MAX 10000
+//#define T_MAX 10000000LL
 
 void * pi(void * void_args);
 
@@ -28,7 +33,7 @@ int main() {
 
     pthread_t tid[4];
 
-    pi_args args[4] = {{ .t = 10000, .ans = 0 }, { .t = 10000, .ans = 0 }, { .t = 10000, .ans = 0 }, { .t = 10000, .ans = 0 }};
+    pi_args args[4] = {{ .t = T_MAX, .ans = 0 }, { .t = T_MAX, .ans = 0 }, { .t = T_MAX, .ans = 0 }, { .t = T_MAX, .ans = 0 }};
 
     int i;
     for (i = 0; i < 4; ++i)
@@ -38,6 +43,7 @@ int main() {
         pthread_join(tid[i], NULL);
 
     long double pi = 0;
+    // Average tests
     for (i = 0; i < 4; ++i)
         pi += args[i].ans;
 
@@ -51,7 +57,23 @@ int main() {
 void * pi(void * void_args) {
     pi_args* args = (pi_args *)void_args;
     
-    args->ans = 0;
+    // Monte Carlo pi: generate 2 rand nums between 0 and 1
+    // If z^2 = x*x + y*y is z <= 1, its within the unit circle
+    // Count number of occurrences within circle and divide by trials and you
+    // have approximated pi/4
+
+    long long i;
+    long double count = 0, x, y, z;
+    for (i = 0; i < args->t; i++) {
+        x = (long double)rand()/RAND_MAX;
+        y = (long double)rand()/RAND_MAX;
+        z = x*x + y*y;
+
+        if (z <= 1)
+            count++;
+    }
+
+    args->ans = count/args->t * 4;
 
     return NULL;
 }
